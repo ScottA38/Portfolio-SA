7f3e628c3d3665c20b105849e80b7cfde4e9ba77
Henry Crofts <hwcrofts@gmail.com>
2017-10-12 12:17:18 +0100

    exercise 1 attempt

---
+++ ex1.py
@@ -0,0 +1,8 @@
+#type 'python3 <scriptname>' in terminal to open correctly
+
+print("Hello World!")
+print('This is exciting.')
+print("Isn't this a thrill?")
+print("I said 'isn't this a thrill?'")
+print("No, you are right it is 'boring'.")
+print("I have added another line...")
