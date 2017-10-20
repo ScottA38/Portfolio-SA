6792227ad16c16286a49d2b9993833a69788e582
ScottA94 <S192660@uos.ac.uk>
2017-10-20 15:27:45 +0100

    Rearranged structure of ex10 now nearly completely procedural except for a function to .lstrip() strings. Using 3 independent 'if' statements within the 100 iteration loop; .lstrip() function on strings and a print(f) statement everything works as intended (I hope).

--- ex10.py
+++ ex10.py
@@ -1,36 +1,29 @@
-#define a function which checks to see if the current iteration number is
-#is a multiple of 3 - takes current iteration no as arg
-def fizz(no):
-    #initialise return variable and set to none
-    fmsg = None
-    #moduko used to check if multiple of 3
-    if no % 3 == 0:
-        #if the number is a multiple of 3 the return variable is set to "Fizz"
-        fmsg = "Fizz"
-    return fmsg
+#a functon call that takes any string argument and strips any whitespace characters
+#including newline to allow fizz and buzz to print in one line when called together
+def strip_whtspace(msg):
+    #call lstrip() function on string arg
+    result = msg.lstrip()
+    #return
+    return result
 
-#function to check if iteration number is a variable of 5 - if so change return string to "Buzz"
-def buzz(no):
-    #initialise return variable and set to 'None' for return value validation later on
-    bmsg = None
-    #modulo used to check if multiple of 5
-    if no % 5 == 0:
-        #if the number is a multiple of 3 the return variable is set to "Fizz"
-        bmsg = "Buzz"
-    return bmsg
-
-#iterate through number 1-100 using 'for'
+#iterate through 10 iterations using 'for' loop and range function
 for no in range(100):
-    #if both functions return non-null data
-    if buzz(no) != None and fizz(no) != None:
-        #print both func return statements on one line
-        print(fizz(no), buzz(no))
-    #if only fizz() returned a string
-    elif fizz(no) != None:
-        print(fizz(no))
-    #if only buzz() returned a string
-    elif buzz(no) != None:
-        print(buzz(no))
-    #if nothing returned a string
-    else:
-        print(no)
+    #reset string variable to hold 'Fizz' to empty for each loop
+    fmsg = ""
+    #reset string variable to hold 'Buzz' to empty for each loop
+    bmsg = ""
+    #if iteration number is a multiple of 3
+    if no % 3 == 0 :
+        #set fmsg variable to "Fizz"
+        fmsg = "Fizz"
+    #if iteration number is a multiple of 5
+    if no % 5 == 0:
+        #set bmsg variable to "Buzz"
+        bmsg = "Buzz"
+    #if both fmsg/bmsg variable strings are empty 
+    if fmsg == "" and bmsg == "":
+        #set fmsg (1st in print statement) to iterator number converted to string
+        fmsg = str(no)
+    fmsg = strip_whtspace(fmsg)
+    bmsg = strip_whtspace(bmsg)
+    print(f"{fmsg}{bmsg}")
