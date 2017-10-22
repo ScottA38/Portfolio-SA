66f0f2880b7864da8e9bd0a200277dbd42253245
ScottA94 <S192660@uos.ac.uk>
2017-10-22 16:34:52 +0100

    2nd upload of ex12.py:

--- ex12.py
+++ ex12.py
@@ -2,6 +2,8 @@
 fixed value for hotel value cost - kept outside of function
 so it can only be referenced and not changed in function scope
 """
+
+#fixed-value integer variable for accomodation cost per night
 accom_per_night = 70
 #set fixed variable for car hire per-day cost (without considering discounts)
 car_hire = 30
@@ -10,8 +12,14 @@ airl_costs = {"New York": 2000, "Auckland": 790, "Venice": 154, "Glasgow": 65}
 #array of class names and correponding
 airl_class = {"Economy": 1.0, "Premium Economy": 1.3, "Business Class": 1.6, "First Class": 1.9}
 
-#define a function which creates an array of indexed values from an existing associative array
 def key_index(array):
+    """
+    Creates a new dict which utilises a numeric index as a key and existing key values
+    from the input dict as the values.
+
+    This function is utilised by data_entry(subject,array) to allow the user
+    to input an integer for selection of data rather than copying a string verbatim.
+    """
     #create an assoc array from the key of the associative array referenced as arg in data_entry
     indexed_array = list(array.keys())
     return indexed_array
@@ -19,8 +27,16 @@ def key_index(array):
 #function template used to individually assign to different variables using
 #subject is the data subject to be selected (such as city), array arg passes list of options as string
 def data_entry(subject, array):
+    """
+    A uniformly applicable function which takes a data subject (simply for user's reference in print
+    statements) and a dict from which the user is required to select a specific value from
+    all existing keys.
 
-    #assign output of key_index(array) function to a specific key list
+    The function notifies user of the task to be undertaken before printing a list of indexes with corresponding
+    values taken from the key of the dict
+    """
+
+    #assign output of key_index(array) function to a specific key list (data type of key_list = 'list')
     key_list = key_index(array)
 
     #iterate through city_index.items()
@@ -58,17 +74,45 @@ def data_entry(subject, array):
 
 #define a function to take arg 'nights' and calculate the cost of a stay
 def hotel_costs(nights):
+    """
+    Take the value for nights (calculated from the amount of days expected stay - 1) that the user is staying at a destination for
+    as an argument (type: integer) and multiply by the fixed accom_per_night value (70 GBP, type: integer) to give a total accomodation
+    cost. Then returns this value.
+    """
+    print("\nHotel cost function:", nights)
     #times fixed accom_per_night value by amount of nights stayed within accomodation - assign to accom_cost
     accom_cost = accom_per_night * nights
     #return accom_cost
     return accom_cost
 
 #define a function which calculates the total cost of airline travel to a destination
-def plane_ticket_cost(city, airl_class):
-    ticket_value = airl_costs[city] * airl_class
+def plane_ticket_cost(city, airl_class_name):
+    """
+    Take the plane selected city name (selected via data_entry(subject,array) function) and airline class name as arguments.
+    Reference these arguments as 'string indicies' inside of airl_costs array and airl_class dicts to return the corresponding
+    value to the key (input args are both dict keys).
+
+    Once these corresponding dict values are returned times them together to find the total air travel costs.
+    """
+    print("\nPlane ticket function:", airl_costs["{}".format(city)])
+    print(airl_class_name)
+    ticket_value = airl_costs[city] * airl_class[airl_class_name]
     return ticket_value
 
 def rental_car_cost(days):
+    """
+    Take the amount of days stay at dest as argument (type: integer). Reference the global variable car_hire (type: integer) to
+    find the potential total value of the car hire.
+
+    Once the total possible value has been found, enter a 2-stage conditional statement to check the length of the rental period,
+    starting with the highest rental period first, followed by the next highest. Apply disount reductions to rental cost through subtraction
+    if conditions are met.
+
+    If the first condition is met, the second cannot also be met due to 'elif' condition (automatically skipped by Python), meaning only
+    one discount can be applied. This method could be applied to a larger range of rental lengths given 1st condition is longest period
+    to validate a discount, followed by the next longest etc.
+    """
+    print("\nRental car cost function:", days)
 
     #times argument integer 'days' by the fixed car hire cost
     rental_cost = car_hire * days
@@ -87,9 +131,13 @@ def rental_car_cost(days):
     return rental_cost
 
 def total_costs(city, days):
+    """
+    Function to call in total cost data from each of functions hotel_costs(nights), rental_car_cost(days) and plane_ticket_cost(city, airl_class_name).
+    Output is printed to user so this is a subroutine (no return value). Gives a breakdown of each indvidual cost (flights, hotel and rental car) before adding everything together.
+    """
     #input both user-specified city and airl_class variables into plane_ticket_cost() function to calculate total airline cost
     #airl_class is global so referencable in this scope.
-    plane_c = plane_ticket_cost(city, airl_class)
+    plane_c = plane_ticket_cost(city, airl_class_name)
 
     #calculate hotel costs based on the user-specified no of nights/days.
     #Argument nights is in global scope so if referenceable within this scope
@@ -100,9 +148,11 @@ def total_costs(city, days):
     rental_c = rental_car_cost(days)
 
     #prints output of each indvidual calculation for cost and all added together
-    print(f"\nThankyou for supplying the requested information.\n\n Based on your inputs I calculate your airline charges will be {plane_c}, your hotel charges will be {hotel_c} and your rental will be {rental_c}")
+    print(f"\nThankyou for supplying the requested information.\n\n Based on your inputs I calculate your airline charges will be {plane_c},\n your hotel charges will be {hotel_c} \nand your rental will be {rental_c}.")
     total_c = plane_c + hotel_c + rental_c
     print(f"This means that the estimated total cost of your trip is {total_c}")
+
+print("Type of airl_costs:", type(airl_costs), "\nType of airl_class: ", type(airl_class))
 
 #welcome message
 print("Welcome to SA's trip travel cost calculator...\n\n\n")
@@ -117,7 +167,12 @@ nights = days - 1
 city = data_entry("city", airl_costs)
 
 #ask user for airline class selection -
-airl_class = data_entry("Airline class", airl_class)
+airl_class_name = data_entry("Airline class", airl_class)
+
+#debug to check the type status of airl_costs and airl_class
+print("Type of airl_costs:", type(airl_costs), "\nType of airl_class: ", type(airl_class))
+#debug to check the contents of airl_class after calling data_entry function
+# print(airl_class)
 
 #call function which calls all relevant associated functions to calulate all other
 total_costs(city, days)
